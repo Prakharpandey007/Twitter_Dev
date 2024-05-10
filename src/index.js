fb844303b3a8b74537c739284f0968af3f72e7c3
prakhar <prakharharshit.2003@gmail.com>
2024-05-10 16:12:09 +0530

    added codes

--- src/index.js
+++ src/index.js
@@ -8,19 +8,13 @@ app.listen(3000, async () => {
   console.log(`server started`);
   await connect();
   console.log("mongodb connected");
-  // const tweet=await  Tweet.create({
-  //     content:'third tweet',
-  //     // userEmail:"a@b.com"
-  // });
-  // const tweets=await Tweet.find({userEmail:"a@b.com"});
+  
   const tweetRepo = new TweetRepository();
-//   const tweet =await tweetRepo.create({content:"tweet with comment schema"})
-//   console.log(tweet);
-// const comment=await Comment.create({content:'new comment'});
-// tweet.comments.push(comment);
-// await tweet.save();
-const tweet=await tweetRepo.getWithComments('661e0b4500b140e7ef004ce3')
-console.log(tweet);
-  // console.log(tweets);
+
+const tweet =await tweetRepo.getAll(0,4);
+
+console.log(tweet[0].contentWithEmail);
+  
 });
+
 
--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -19,7 +19,7 @@ try {
 }
 async getWithComments(id){
     try {
-        const tweet=await Tweet.findById(id).populate({path:'comments'});
+        const tweet=await Tweet.findById(id).populate({path:'comments'}).lean();
         return tweet;
 
     } catch (error) {
@@ -45,6 +45,17 @@ try {
     console.log(error);
 }
 }
+async getAll(offset,limit){
+    try {
+        const tweet=await Tweet.find().skip(offset).limit(limit);
+        return tweet;
+    
+        
+    } catch (error) {
+        console.log(error);
+    }
 }
+}
+
 
 module.exports=TweetRepository;
