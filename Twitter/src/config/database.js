6a42f9a89383d7a1965d53f8083f5a169b0c120d
prakhar <prakharharshit.2003@gmail.com>
2025-06-05 08:28:31 +0530

    fix the database.js file

--- Twitter/src/config/database.js
+++ Twitter/src/config/database.js
@@ -1,7 +1,21 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 export const connect = async () => {
-  await mongoose.connect(
-    "mongodb+srv://prakharharshit2003:7trPLWURKfmdcNto@cluster0.coxirc7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  );
+  try {
+    const mongoURI = process.env.MONGODB_URI;
+
+    if (!mongoURI) {
+      throw new Error("MONGODB_URI is not defined in .env file");
+    }
+
+    await mongoose.connect(mongoURI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1); // Exit with failure
+  }
 };
